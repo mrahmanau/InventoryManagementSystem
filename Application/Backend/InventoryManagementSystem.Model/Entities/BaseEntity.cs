a6889f26cadf3b2a629bc781c7264fee0b7e3850
Mahfuzur Rahman <mrahmanlinks@gmail.com>
2024-05-24 23:32:33 -0300

    Added Entities

---
+++ Application/Backend/InventoryManagementSystem.Model/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Model
+{
+    public abstract class BaseEntity
+    {
+        public List<ValidationError> Errors { get; set; } = new();
+
+        public void AddError(ValidationError error)
+        {
+            Errors.Add(error);
+        }
+    }
+}
