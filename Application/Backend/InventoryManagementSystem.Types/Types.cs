4536b1b9e5f227d562dfe7f0295392aff189b4be
Mahfuzur Rahman <mrahmanlinks@gmail.com>
2024-05-24 23:03:31 -0300

    Added the Types Class Library

---
+++ Application/Backend/InventoryManagementSystem.Types/Types.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+
+namespace InventoryManagementSystem.Types
+{
+    public class Parm
+    {
+        public string? Name { get; set; }
+        public SqlDbType DataType { get; set; }
+        public object? Value { get; set; }
+        public int Size { get; set; }
+        public ParameterDirection Direction { get; set; }
+
+        public Parm(string? name, SqlDbType dataType, object? value, int size = 0, ParameterDirection direction = ParameterDirection.Input)
+        {
+            Name = name;
+            DataType = dataType;
+            Value = value;
+            Size = size;
+            Direction = direction;
+        }
+    }
+
+    public enum ErrorType
+    {
+        Model, Business
+    }
+}
