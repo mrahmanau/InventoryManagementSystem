1996b4259a2977e033418a978c8c75ed3ccdfb66
Mahfuzur Rahman <mrahmanlinks@gmail.com>
2024-05-30 00:39:02 -0300

    Angular - Implemented the Register Functionality

---
+++ Application/Frontend/inventorymanagemetnsystem/src/app/components/auth/register/register.component.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AuthService } from '../../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.registerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      username: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      roleId: ['2', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (res) => {
+          alert('User registered successfully');
+          this.registerForm.reset();
+        },
+        error: (err) => {
+          alert('Error: ' + err.error.message);
+        },
+      });
+    }
+  }
+}
